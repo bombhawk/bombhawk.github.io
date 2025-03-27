f6f9a7a2dd28873ded7c1d1f7e59427d655acb44
Kamryn <147673746+bombhawk@users.noreply.github.com>
2025-03-27 13:19:05 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,27 @@
 //Data file
 //Magnum Opuses Array
 let opuses = [
-  {link:"GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I developed during the summer of 2024."},
-  {link:"Software/wordle/wordle.html", image:"Images/croppedUnderConstruction.png", name:"Wordle (2025)", description:"A remake of the popular online puzzle game Wordle."},
-  {link:"Software/robot.html", image:"Images/croppedRobot.png", name:"Project Spyn Robot (2023)", description:"A maze solving robot I programmed and constructed in a small team for my intro to engineering class."},
-  {link:"Software/passGen/passwordGenerator.html", image:"Images/passGenCover.png", name:"Password Generator (2022)", description:"The program I made as my Performance Task for AP Computer Science Principles."},
+  {link:"/GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I developed during the summer of 2024."},
+  {link:"/Software/wordle/wordle.html", image:"Images/wordleThumbnail.png", name:"Wordle (2025)", description:"A remake of the popular online puzzle game Wordle."},
+  {link:"/Software/robot.html", image:"Images/croppedRobot.png", name:"Project Spyn Robot (2023)", description:"A maze solving robot I programmed and constructed in a small team for my intro to engineering class."},
+  {link:"/Software/passGen/passwordGenerator.html", image:"Images/passGenCover.png", name:"Password Generator (2022)", description:"The program I made as my Performance Task for AP Computer Science Principles."},
 ];
 
 //Games array
 let games = [
-  {link:"GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I developed during the summer of 2024."},
-  {link:"GamesPages/paintGame.html", image:"Images/WarholWareCoverArt.png", name:"Warhol Ware (2022)", description:"A collection of mini-games I made in 48 hours using MS Paint for all art in 2022."},
-  {link:"GamesPages/christmas.html", image:"Images/christmasCover.png", name:"The Fight Before Christmas (2020)", description:"A game I made working daily during December of 2020."},
-  {link:"GamesPages/mazeMan.html", image:"Images/mazeMan cover.png", name:"Maze-Man (2019-2020)", description:"The first game I completed back in 2019. I then spent some time overhauling and improving it in 2020."}  
+  {link:"/GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I developed during the summer of 2024."},
+  {link:"/GamesPages/paintGame.html", image:"Images/WarholWareCoverArt.png", name:"Warhol Ware (2022)", description:"A collection of mini-games I made in 48 hours using MS Paint for all art in 2022."},
+  {link:"/GamesPages/christmas.html", image:"Images/christmasCover.png", name:"The Fight Before Christmas (2020)", description:"A game I made working daily during December of 2020."},
+  {link:"/GamesPages/mazeMan.html", image:"Images/mazeMan cover.png", name:"Maze-Man (2019-2020)", description:"The first game I completed back in 2019. I then spent some time overhauling and improving it in 2020."}  
 ];
 
 //Software array
 let software = [
-  {link:"Software/wordle/wordle.html", image:"Images/croppedUnderConstruction.png", name:"Wordle (2025)", description:"A remake of the popular online puzzle game Wordle."},
-  {link:"Software/toDoList/toDo.html", image:"Images/croppedToDo.png", name:"To Do List (2025)", description:"A small to-do list program I made in about an hour to warm up my Javascript."},
-  {link:"Software/circuit.html", image:"Images/croppedCircuit.png", name:"Automatic Pool Water Leveler Circuit (2024)", description:"The final project for my EEE120 intro to circuits class."},
-  {link:"Software/robot.html", image:"Images/croppedRobot.png", name:"Project Spyn Robot (2023)", description:"A maze solving robot I programmed and constructed in a small team for my intro to engineering class."},
-  {link:"Software/passGen/passwordGenerator.html", image:"Images/passGenCover.png", name:"Password Generator (2022)", description:"The program I made as my Performance Task for AP Computer Science Principles."},
+  {link:"/Software/wordle/wordle.html", image:"Images/wordleThumbnail.png", name:"Wordle (2025)", description:"A remake of the popular online puzzle game Wordle."},
+  {link:"/Software/toDoList/toDo.html", image:"Images/croppedToDo.png", name:"To Do List (2025)", description:"A small to-do list program I made in about an hour to warm up my Javascript."},
+  {link:"/Software/circuit.html", image:"Images/croppedCircuit.png", name:"Automatic Pool Water Leveler Circuit (2024)", description:"The final project for my EEE120 intro to circuits class."},
+  {link:"/Software/robot.html", image:"Images/croppedRobot.png", name:"Project Spyn Robot (2023)", description:"A maze solving robot I programmed and constructed in a small team for my intro to engineering class."},
+  {link:"/Software/passGen/passwordGenerator.html", image:"Images/passGenCover.png", name:"Password Generator (2022)", description:"The program I made as my Performance Task for AP Computer Science Principles."},
 ];
 
 /*
