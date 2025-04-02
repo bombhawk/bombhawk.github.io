c8e4b12cc3ccaba9d5025dd5fb1ecaa06e325e44
Kamryn <147673746+bombhawk@users.noreply.github.com>
2025-04-02 14:21:38 -0700

    Bullet Hell Description Fix

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 //Data file
 //Magnum Opuses Array
 let opuses = [
-  {link:"/GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I developed during the summer of 2024."},
+  {link:"/GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I started developing during the summer of 2024."},
   {link:"/Software/wordle/wordle.html", image:"Images/wordleThumbnail.png", name:"Wordle (2025)", description:"A remake of the popular online puzzle game Wordle."},
   {link:"/Software/robot.html", image:"Images/croppedRobot.png", name:"Project Spyn Robot (2023)", description:"A maze solving robot I programmed and constructed in a small team for my intro to engineering class."},
   {link:"/Software/passGen/passwordGenerator.html", image:"Images/passGenCover.png", name:"Password Generator (2022)", description:"The program I made as my Performance Task for AP Computer Science Principles."},
@@ -9,7 +9,7 @@ let opuses = [
 
 //Games array
 let games = [
-  {link:"/GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I developed during the summer of 2024."},
+  {link:"/GamesPages/bulletHell.html", image:"Images/marcArt.jpg", name:"The Good, The Bad, and The Pixely (2024-2025)", description:"A top-down shooter with an emphasis on boss fights I started developing during the summer of 2024."},
   {link:"/GamesPages/paintGame.html", image:"Images/WarholWareCoverArt.png", name:"Warhol Ware (2022)", description:"A collection of mini-games I made in 48 hours using MS Paint for all art in 2022."},
   {link:"/GamesPages/christmas.html", image:"Images/christmasCover.png", name:"The Fight Before Christmas (2020)", description:"A game I made working daily during December of 2020."},
   {link:"/GamesPages/mazeMan.html", image:"Images/mazeMan cover.png", name:"Maze-Man (2019-2020)", description:"The first game I completed back in 2019. I then spent some time overhauling and improving it in 2020."}  
