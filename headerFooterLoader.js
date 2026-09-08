0c7a989ecc8f50a8497e83822b9fb846946a9c13
Kamryn <kamkramer@gmail.com>
2026-09-07 22:25:06 -0700

    Centralized header and fixed numerous small errors

--- headerFooterLoader.js
+++ headerFooterLoader.js
@@ -1,3 +1,10 @@
+//Loads header at the top of each page
+fetch("/header.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("headerFillIn").innerHTML = data;
+    });
+
 //Loads footer at the bottom of each page
 fetch("/footer.html")
     .then(response => response.text())
